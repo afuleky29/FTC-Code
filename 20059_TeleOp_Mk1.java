7dc4e6c6ef15ec9ae05ceb9f8e3a1f390ce3380b
WaddlesGamer <ndurant29@punahou.edu>
2026-09-01 15:38:26 -1000

    more telemetry and settings

--- 20059_TeleOp_Mk1.java
+++ 20059_TeleOp_Mk1.java
@@ -7,31 +7,63 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "_20059TeleOpMk1", group = "Linear OpMode")
 public class _20059TeleOpMk1 extends LinearOpMode {
 
+telemetry.addLine("A/B for On/Off Instructions");
+telemetry.addLine("X/Y for On/Off ZPB");
+
+//for example vArIaBlEs
+boolean instructions = true; //aturn on off instructions
+boolean ZPB = true; //turn on off fast stop (Zero Power Behavior)
+int speed = 1; //1:slow 2:fast
+//motor definitions
+DcMotor FL;//namehere//
+DcMotor FR;
+DcMotor BL;
+DcMotor BR;
+//map motor to variable
+FL = hardwareMap.get(DcMotor.class, "FL");
+FR = hardwareMap.get(DcMotor.class, "FR");
+BL = hardwareMap.get(DcMotor.class, "BL");
+BR = hardwareMap.get(DcMotor.class, "BR");
+telemetry.addData("Instructions", instructions)
+telemetry.addData("ZPB", ZPB)
+telemetry.update();
+//HERE TO 
+//wayyyy overcomplicated options
+  @Override
+  public void init_loop() {
+    if (gamepad1.b) {
+      boolean instructions = false;
+      telemetry.update();
+    }
+    if (gamepad1.a) {
+      boolean instructions = true;
+      telemetry.update();
+    }
+    if (gamepad1.y) {
+      boolean ZPB = false;
+      telemetry.update();
+    }
+    if (gamepad1.x) {
+      boolean ZPB = true;
+      telemetry.update();
+    }
+  }
+//HERE IS UNESSARCY BUT COOL*/
+
   @Override
   public void runOpMode() {
     /*INITIALIZATION*/
     //RUNS ONCE on INT (Before run)
-    //for example vArIaBlEs
-    int speed = 1; //1:slow 2:fast
-    boolean instructions = true; //turn on off instructions
+    telemetry.clear();
     if (instructions == true) {
       telemetry.addData("CSM stands for 'Current Speed Mode', the gamepad 1 leftY and leftX are for debugging, nevermind them");
     }
+    telemetry.addData("ZPB", ZPB)
 
     waitForStart();
     if (opModeIsActive()); {
       /*PLAY ONCE*/
       // RUNS ONCE on PLAY (After run)
-      //motor definitions
-      DcMotor FL;//namehere//
-      DcMotor FR;
-      DcMotor BL;
-      DcMotor BR;
-      //map motor to variable
-      FL = hardwareMap.get(DcMotor.class, "FL");
-      FR = hardwareMap.get(DcMotor.class, "FR");
-      BL = hardwareMap.get(DcMotor.class, "BL");
-      BR = hardwareMap.get(DcMotor.class, "BR");
       
       while (opModeIsActive()) {
         /*PLAY FOREVER*/
@@ -61,6 +93,12 @@ public class _20059TeleOpMk1 extends LinearOpMode {
           FR.setPower(0);
           BL.setPower(0);
           BR.setPower(0);
+          if (ZPB == true) {
+            FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+          }
         }
         
         if (rightX != 0) {
